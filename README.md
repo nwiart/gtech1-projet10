26781f0fc04cae65f762a641bcc94cb25b42af4e
nwiart <nwiart@gaming.tech>
2022-03-16 10:35:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gtech1-projet10
+
+Projet de site web avec NextJS
+
---
+++ app/pages/tonton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const b = () => {
+	return (
+		<div>
+			idfbgdfighbn
+		</div>
+	);
+}
+
+export default b;
